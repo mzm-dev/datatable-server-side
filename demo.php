2f911d8bb7fbe6911d296c53d5587ebdee0b9980
Mohamad Zaki Mustafa <mohamadzakimustafa@gmail.com>
2021-12-23 16:15:02 +0800

    Add button

--- demo.php
+++ demo.php
@@ -7,16 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DataTable Post List Processing</title>
 
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
 </head>
 
 <body>
+
+
     <div class="container">
 
         <h1>DataTable Post List Processing</h1>
-
 
         <table id="post_list" class="table table-striped" style="width:100%">
             <thead>
@@ -26,9 +29,9 @@
                     <th>Last Name</th>
                     <th>Email</th>
                     <th>Gender</th>
+                    <th>Action</th>
                 </tr>
             </thead>
-
         </table>
 
     </div>
@@ -38,33 +41,46 @@
     <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
 
     <script>
-        $('#post_list').dataTable({
-            "bProcessing": true,
-            "serverSide": true,
-            "ajax": {
-                url: "server_post_list.php",
-                type: "POST",
-                error: function() {
-                    $("#post_list_processing").css("display", "none");
-                }
-            },
-            "columns": [{
-                    'data': 'id'
-                },
-                {
-                    'data': 'first_name'
-                },
-                {
-                    'data': 'last_name'
-                },
-                {
-                    'data': 'email'
-                },
-                {
-                    'data': 'gender'
-                },
-            ]
+        $(document).ready(function() {
 
+
+            var htmlBtn = '<div class="btn-group" role="group" aria-label="Basic example">';
+            htmlBtn += '<button type="button" class="one btn btn-info btn-sm"><i class="fas fa-search"></i></button>';
+            htmlBtn += '<button type="button" class="two btn btn-warning btn-sm"><i class="fas fa-pencil-alt"></i></button>';
+            htmlBtn += '<button type="button" class="three btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>';
+            htmlBtn += '</div>';
+
+            var table = $('#post_list').DataTable({
+                "bProcessing": true,
+                "serverSide": true,
+                "ajax": {
+                    url: "server_post_list.php",
+                    type: "POST",
+                    error: function() {
+                        $("#post_list_processing").css("display", "none");
+                    }
+                },
+                "columnDefs": [{
+                    "targets": -1,
+                    "data": null,
+                    "defaultContent": htmlBtn
+                }]
+            });
+
+            $('#post_list tbody').on('click', 'button.one', function() {
+                var data = table.row($(this).parents('tr')).data();
+                alert("ID : "+data[0]+" 'Data: " + data[1]);
+            });
+
+            $('#post_list tbody').on('click', 'button.two', function() {
+                var data = table.row($(this).parents('tr')).data();
+                alert("ID : "+data[0]+" 'Data: " + data[2]);
+            });
+            
+            $('#post_list tbody').on('click', 'button.three', function() {
+                var data = table.row($(this).parents('tr')).data();
+                alert("ID : "+data[0]+" 'Data: " + data[3]);
+            });
         });
     </script>
 </body>
